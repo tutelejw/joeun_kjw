7ec4d66a2a72c1a33e6fe292bbe7a46ceba7afdf
papa <papa@LAPTOP-G5DCD446>
2025-07-13 01:10:59 +0900

    333

--- HomeWork_07/src/m07/day14/test.java
+++ HomeWork_07/src/m07/day14/test.java
@@ -3,5 +3,7 @@ package m07.day14;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
-	}
+		System.out.println("test");
+		System.out.println("test");
+		}
 }
