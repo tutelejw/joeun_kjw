ca6bcd59bd275ca17ef314c76d12de203a478154
home_3600 <home_3600@DESKTOP-PARCR2F>
2025-07-14 01:24:49 +0900

    666

---
+++ HomeWork_07/src/kjw/m07/d14/CellPhone.java
@@ -0,0 +1,50 @@
+package kjw.m07.d14;
+
+public class CellPhone {
+	private String phoneNumber;  //전화번호000-000-0000
+	private int totalCallTime; //전화사용시간 10 100 200
+	private int totalFee; //사용 요금
+	private int feePerCallTime;  //시간당 사용요금.
+	
+	public CellPhone() {
+	}
+	public CellPhone(String phoneNumber, int feePerCallTime) { //전화번호, 전화사용시간.
+		this.phoneNumber=phoneNumber;
+		this.feePerCallTime=feePerCallTime;	
+	}
+	
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public int getTotalCallTime() {
+		return totalCallTime;
+	}
+	public int getTotalFee() {
+		return totalFee;
+	}
+	public int getFeePerCallTime() {
+		return feePerCallTime;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber=phoneNumber;
+	}
+	public void setTitalCallTime(int totalCallTime) {
+		this.totalCallTime=totalCallTime;
+	}
+	public void setTotalFee (int totalFee) {
+		this.totalFee = totalFee;
+	}
+	public void setFeePerCallTime(int feePerCallTime) {
+		this.feePerCallTime=feePerCallTime;
+	}
+	
+	public void call(String phoneNumber, int totalCallTime) {
+		this.totalCallTime += totalCallTime;
+		System.out.println(phoneNumber + " 번호로 " +totalCallTime + " 통화함.");
+	}
+	public int calculateTotalFee() {
+		this.totalFee = this.totalCallTime * this.feePerCallTime;
+		return this.totalFee;
+	}
+}
--- HomeWork_07/src/kjw/m07/d14/Prob3.java
+++ HomeWork_07/src/kjw/m07/d14/Prob3.java
@@ -6,23 +6,23 @@ public class Prob3 {
 		cellPhone.call("016-016-016", 10);
 		cellPhone.calculateTotalFee();
 		System.out.println(cellPhone.getPhoneNumber() + " 통화 시간: " + cellPhone.getTotalCallTime()
-				+ ", 단가: " + cellPhone.getFeePerCallTime() + ", 총 요금: " + cellPhone.getTotalFee() + "원");
+				+ ", 단가: " + cellPhone.getFeePerCallTime() + ", 총 요금: " + cellPhone.getTotalFee() + " 원");
 
 		System.out.println("========================================");
 
-		SmartPhone smartPhone = new SmartPhone("000-000-000", 200);
-		smartPhone.call("017-017-017", 10);
-		smartPhone.calculateTotalFee();
-		System.out.println(smartPhone.getPhoneNumber() + " 통화 시간: " + smartPhone.getTotalCallTime()
-				+ ", 단가: " + smartPhone.getFeePerCallTime() + ", 총 요금: " + smartPhone.getTotalFee() + "원");
-
-		System.out.println("========================================");
-
-		SmartPhone smartPhone01 = new SmartPhone("111-111-111", 0, true, 3000);
-		smartPhone01.call("017-017-017", 10);
-		smartPhone01.call("016-016-016", 10);
-		smartPhone01.calculateTotalFee();
-		System.out.println(smartPhone01.getPhoneNumber() + " 통화 시간: " + smartPhone01.getTotalCallTime()
-				+ ", 단가: " + smartPhone01.getFeePerCallTime() + ", 총 요금: " + smartPhone01.getTotalFee() + "원");
+//		SmartPhone smartPhone = new SmartPhone("000-000-000", 200);
+//		smartPhone.call("017-017-017", 10);
+//		smartPhone.calculateTotalFee();
+//		System.out.println(smartPhone.getPhoneNumber() + " 통화 시간: " + smartPhone.getTotalCallTime()
+//				+ ", 단가: " + smartPhone.getFeePerCallTime() + ", 총 요금: " + smartPhone.getTotalFee() + "원");
+//
+//		System.out.println("========================================");
+//
+//		SmartPhone smartPhone01 = new SmartPhone("111-111-111", 0, true, 3000);
+//		smartPhone01.call("017-017-017", 10);
+//		smartPhone01.call("016-016-016", 10);
+//		smartPhone01.calculateTotalFee();
+//		System.out.println(smartPhone01.getPhoneNumber() + " 통화 시간: " + smartPhone01.getTotalCallTime()
+//				+ ", 단가: " + smartPhone01.getFeePerCallTime() + ", 총 요금: " + smartPhone01.getTotalFee() + "원");
 	} // end of main
 } // end of class
