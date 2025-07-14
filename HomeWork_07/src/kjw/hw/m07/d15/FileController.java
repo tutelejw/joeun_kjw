f0b1cb91863b2bd22cb57ba8632f5b15838b3237
TJ <TJ@DESKTOP-4HEBAE3>
2025-07-14 15:37:07 +0900

    33

--- HomeWork_07/src/kjw/hw/m07/d15/FileController.java
+++ HomeWork_07/src/kjw/hw/m07/d15/FileController.java
@@ -1,5 +1,19 @@
 package kjw.hw.m07.d15;
 
 public class FileController {
+	public void reverseLine(String oldf, String newf) throws Exception{
+		//reverseLine 메서드 구현.
+		System.out.println("reverseLine 메서드 구현.");
+	} // out of method
+	
+	public static void main(String[] args) throws Exception{
+		FileController c = new FileController();
+		String oldf = "files/oldf.txt";
+		String newf = "files/newf.txt";
+		c.reverseLine(oldf,newf);
+	}//out of main
+} //out of classs
 
-}
+//reverseLine 메서드 구현
+//String 타입의 텍스트 파일명을 입력받아 텍스트의 라인순서를 반대로하여 두번째 인자인 출력할 String 타입의 텍스트 파일명으로 저장
+ 
