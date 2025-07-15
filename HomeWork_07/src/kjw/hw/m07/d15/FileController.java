ebc68df89624a6f2f8ddbb12e3d0e27539570632
TJ <TJ@DESKTOP-4HEBAE3>
2025-07-15 19:31:48 +0900

    ㄹㄹㄹㄹ

--- HomeWork_07/src/kjw/hw/m07/d15/FileController.java
+++ HomeWork_07/src/kjw/hw/m07/d15/FileController.java
@@ -1,27 +1,36 @@
 package kjw.hw.m07.d15;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+//import java.io.*;
+//import java.util.*;
 
 public class FileController {
 	public void reverseLine(String oldf, String newf) throws Exception{
 		//reverseLine 메서드 구현.
 		//라인 수 세기
-		BufferedReader br = new BufferedReader(new FileReader(oldf));
+		//FileReader fileReader = new FileReader(oldf);
+		//BufferedReader br = new BufferedReader(fileReader); 
+		BufferedReader br = new BufferedReader(new FileReader(oldf)); //oldf 파일 br로 선언 초기화
 		List<String>m = new ArrayList<String>();
 		
 		String line = null;
-		while ((line = br.readLine())!=null) {
-			m.add(line);
-		}
-		br.close();
+		while ((line = br.readLine())!=null) { //라인을 읽어서 null 이 아니면 
+			m.add(line);  //line을  ArrayList의 add 메서드로  m 배열에 추가..
+		}  //마지막줄에 null 만나서 종료
+		br.close();  // 파일 읽기가 끝나서 bufferedreader 종료
 		
-		PrintWriter pw = new PrintWriter(new FileOutputStream(newf));
-		for (int i=m.size() - 1; i>=0; i--) {
-			pw.println(m.get(i));
+		PrintWriter pw = new PrintWriter(new FileWriter(newf)); 
+		for (int i=m.size() - 1; i>=0; i--) {   // size = List interface의 메서드 
+			pw.println(m.get(i));  //newf 파일의 라인을 역순으로 가져오기 get = List interface의 메서드
 		}
-		pw.flush();
-		pw.close();	
+		pw.flush();  //println()은 데이터를 버퍼에 쌓아두고, flush()는 그 데이터를 실제로 파일에 기록
+		pw.close();	 //pw 파일 쓰기 완료 후 종료
 		
 		System.out.println("reverseLine 메서드 구현.");
 	} // out of method
