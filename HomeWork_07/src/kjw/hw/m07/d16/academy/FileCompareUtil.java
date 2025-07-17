1a7881dc4463efe1cd0cf1ec95036af90ab30b31
TJ <TJ@DESKTOP-4HEBAE3>
2025-07-17 18:00:13 +0900

    jkl

--- HomeWork_07/src/kjw/hw/m07/d16/academy/FileCompareUtil.java
+++ HomeWork_07/src/kjw/hw/m07/d16/academy/FileCompareUtil.java
@@ -1,17 +1,38 @@
 package kjw.hw.m07.d16.academy;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 //java.io, java.util  api 연습
 public class FileCompareUtil {
+	public ArrayList<String> compareFile(String fstFileName, String scdFileName) throws Exception{
+		//implements compareFile method.
+		BufferedReader fstBr = new BufferedReader(new FileReader(fstFileName));
+		BufferedReader scdBr = new BufferedReader(new FileReader(scdFileName));
+		
+		String fstLine = fstBr.readLine();
+		String scdLine = scdBr.readLine();
+		
+		int lineNum = 0;
+		ArrayList<String>resultList = new ArrayList<String>();
+		
+		while (fstLine != null) {
+			lineNum++;
+			if(!fstLine.equals(scdLine)) {
+				resultList.add("LINE" + lineNum + ":" + scdLine);
+			}
+			fstLine = fstBr.readLine();
+			scdLine = scdBr.readLine();
+			
+		}
+		return resultList;
+	}
+	
 	public static void main(String[] args) throws Exception{
 		FileCompareUtil util = new FileCompareUtil();
-		System.out.println(util.compareFile("./files/fstFile1.txt", "./filex/scdFile1.txt"));
+		System.out.println(util.compareFile("./files/fstFile1.txt", "./files/scdFile1.txt"));
 		System.out.println("Successful!!!");
 	}//end of main
 	
-	public ArrayList compareFile(String fstFileName, String scdFileName) throws Exception{
-		//implements compareFile method.
-		return null;
-	}
 } // end of class
