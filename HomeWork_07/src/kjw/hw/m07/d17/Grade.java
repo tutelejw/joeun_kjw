00fd16a6da56c46bd296a5c3a4688170d6c32469
TJ <TJ@DESKTOP-4HEBAE3>
2025-07-17 18:40:32 +0900

    45

---
+++ HomeWork_07/src/kjw/hw/m07/d17/Grade.java
@@ -0,0 +1,13 @@
+package kjw.hw.m07.d17;
+
+public class Grade {
+	public static void main(String[] args) {
+	Grade grade = new Grade();
+	String fileName = "./files/score.txt";
+	grade.printGrade(fileName);
+	}//out of main
+	
+	public void printGrade(String fileName) {
+		
+	}
+}//out of class
