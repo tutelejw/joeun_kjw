0e45fcbae122813ada2a71c218b222a90ee90c34
home_3600 <home_3600@DESKTOP-PARCR2F>
2025-07-18 00:50:56 +0900

    fd

---
+++ CodingTraining/src/BookApp.java
@@ -0,0 +1,17 @@
+
+public class BookApp {
+	
+	class Book{
+	String title ; //제목
+	String author; //저자
+	int price ; //가격
+	public Book() {
+	}
+	public Book(String title, String author, int price) {
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+	
+	}//out of Bean
+}// out of class
