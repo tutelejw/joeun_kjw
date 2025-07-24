043eee295cd36ffe311134447047c0a53a2ac975
TJ <TJ@DESKTOP-08FKNBG>
2025-07-24 10:18:21 +0900

    fdffd

---
+++ HomeWork_07/src/kjw/hw/m07/d18_oracle/EmpStatistics.java
@@ -0,0 +1,51 @@
+package kjw.hw.m07.d18_oracle;
+import java.sql.*;
+
+public class EmpStatistics{
+	private static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	private static final String user = "hr";
+	private static final String pass = "hr";
+
+	//1단계 : Connection 
+	static {
+		try {
+			Class.forName(ORACLE_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public void printStatistics(int maxSalary) throws SQLException {
+		System.out.println("=================================");
+		System.out.println("Max Salary: " + maxSalary);
+		System.out.println("=================================");
+
+		Connection con = DriverManager.getConnection(url,user,pass);
+
+		String sql = "SELECT j.job_title, ROUND(AVG(NVL(e.salary, 0)), 0) 평균급여 FROM employees e, jobs j "
+				+ "WHERE e.job_id = j.job_id AND e.salary >= ? "
+				+ "GROUP BY j.job_title "
+				+ "ORDER BY ROUND(AVG(NVL(e.salary, 0)), 0) DESC";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+
+		pstmt.setInt(1, maxSalary);
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+			System.out.println("[" + rs.getString("job_title") + "] " + rs.getInt("평균급여"));
+		}
+
+		if (rs != null) rs.close();
+
+		if (pstmt != null) pstmt.close();
+
+		if (con != null) con.close();
+	}
+
+	/// Main
+	public static void main(String[] args) throws SQLException {
+		new EmpStatistics().printStatistics(10000);
+		new EmpStatistics().printStatistics(15000);
+	    
+	}//end of main
+}//end of class
---
+++ HomeWork_07/src/kjw/hw/m07/d18_oracle/Example01.java
@@ -0,0 +1,29 @@
+package kjw.hw.m07.d18_oracle;
+import java.sql.*;
+
+public class Example01{
+
+	public static void main(String[] args) throws Exception {
+		
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+
+		//1단계 : Connection 
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url,"scott","tiger");
+		
+		//2단계 : Statement 
+		Statement stmt = con.createStatement();
+		
+		//3단계 : Query문 전송 및 결과 return
+		String createSql = "CREATE TABLE MEMBER(no NUMBER, id VARCHAR2(10), pwd VARCHAR2(10))";
+		
+		stmt.executeUpdate(createSql);
+		
+		System.out.println("number TABLE 생성완료");
+
+	    if (stmt != null) 		stmt.close();
+	    if (con != null)			con.close();
+	    
+	}//end of main
+	
+}//end of class
