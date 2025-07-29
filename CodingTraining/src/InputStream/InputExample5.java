4877ba05c450a063bb95b46964fe7cf41b05d89f
home_3600 <home_3600@DESKTOP-PARCR2F>
2025-07-29 23:07:51 +0900

    ff

---
+++ CodingTraining/src/InputStream/InputExample5.java
@@ -0,0 +1,36 @@
+package InputStream;
+
+
+import java.io.IOException;
+
+public class InputExample5 {
+	public static void main(String[] args) {
+		System.out.print("한글자 씩 입력 : ");
+		int ch;
+		
+		try {
+			while ((ch = System.in.read()) !=  -1) {
+				if(ch == 'q') {
+					break;
+				}
+				if(ch == '\n' || ch == '\r') {
+					continue;
+				}
+				System.out.println("입력 값 : " + (char)ch);	
+			}
+			System.out.println("종료함다.");
+		}catch(IOException e) {
+			System.out.println("에러 발생 " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+//try {
+//	int input = System.in.read();
+//	System.out.println("입력 값 : " + (char)input);
+//}catch(IOException e) {
+//	System.out.println("에러 발생 " + e.getMessage());
+//	e.printStackTrace();
+//}
---
+++ CodingTraining/src/InputStream/InputExample7.java
@@ -0,0 +1,25 @@
+package InputStream;
+
+import java.io.IOException;
+
+public class InputExample7 {
+	public static void main(String[] args) {
+		byte[] buffer = new byte[20];
+		System.out.print("입력 ㅣ(exit입력종료): ");
+		
+		try {
+			while(true) {
+				int len = System.in.read(buffer);
+				String input = new String(buffer,0, len).trim();
+				System.out.println("len 값 : " + len);
+				if(input.equals("exit")) {
+					break;
+				}
+				System.out.println(" 출력 : " + input);
+			}
+		}catch(IOException e){
+			System.out.println("에러에러" + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
