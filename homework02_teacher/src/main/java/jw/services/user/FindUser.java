02e29e104ace150181c2fc6d7a3cc8d5ca7f5421
TJ <TJ@DESKTOP-08FKNBG>
2025-08-04 10:21:09 +0900

    cda

---
+++ homework02_teacher/src/main/java/jw/services/user/FindUser.java
@@ -0,0 +1,63 @@
+package jw.services.user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jw.services.user.dao.UserDAO;
+import jw.services.user.vo.UserVO;
+
+public class FindUser extends HttpServlet {
+	
+	//doGet() Overriding
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+	
+	//doPost() Overriding
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = res.getWriter();
+		
+		//client에서 넘어온 값을 받자.
+		String id = req.getParameter("id");
+		
+		////////////////////////  Session 사용으로 추가된 부분 ///////////////////////////////////////
+		//==>id가 null인경우는 ?
+		//==><a href='/homework02/FindUser'>내정보보기2(session사용)</a>
+		if(id == null){
+			//==>  Session에 저장된 userVO instance 의 id 사용
+			HttpSession session = req.getSession(true);
+			id =  ( (UserVO)session.getAttribute("userVO")).getId();
+		}
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		//DB에 접근하는 UserDAO를 이용 회원정보 select 후
+		UserDAO bean = new UserDAO();
+		//==> 회원의 정보를 갖는 userVO intance로 받기
+		UserVO userVO = bean.findUser(id);
+
+		out.println("<html>");
+		out.println("<body>");
+		
+		out.println("<h2>요청하신 회원정보</h2>");
+        
+		if(userVO != null){
+            out.println("no  : "+ userVO.getNo()+" <br/>");
+            out.println("id   : "+ userVO.getId()+" <br/>");
+            out.println("pwd : "+ userVO.getPwd()+" <br/>");
+		}else{
+			out.println(id +"에 해당하는 data는 없습니다.<br/>");
+		}
+
+		out.println("</boyd>");
+		out.println("</html>");
+	}
+}
