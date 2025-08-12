ff0857ee252734d815733ac348a2229df0587ed7
tj <tj@DESKTOP-TT4MJRK>
2025-08-12 15:26:31 +0900

    fdfd

--- 02.Model2MVCShop_Refactor_PageNavi/src/main/java/com/model2/mvc/common/Page.java
+++ 02.Model2MVCShop_Refactor_PageNavi/src/main/java/com/model2/mvc/common/Page.java
@@ -12,7 +12,7 @@ public class Page {
 	private int beginUnitPage;	//화면에 보여지는 페이지 번호의 최소수
 	private int endUnitPage;	//화면에 보여지는 페이지 번호의 최대수
 	
-	///Constructor
+	///Constructor 
 	public Page() {
 	}
 	public Page( int currentPage, int totalCount,	int pageUnit, int pageSize ) {
