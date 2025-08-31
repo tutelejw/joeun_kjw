681cbc8503e16f050f4bcba86f4dd85654c3a8d7
home_3600 <home_3600@DESKTOP-PARCR2F>
2025-08-31 20:16:58 +0900

    g

---
+++ study0830/src/study0830/Robot.java
@@ -0,0 +1,37 @@
+package study0830;
+
+interface Movable{
+	void move();
+}
+
+interface Activable{
+	void activate();
+}
+
+class Robot_imp implements Movable, Activable{
+	private String name;
+	public Robot_imp(String name) {
+		this.name=name;
+	}
+	public void move() {
+		System.out.println(name  + " 가 지정된 경로로 이동합니다.");
+	}
+	public void activate() {
+		System.out.println(name  + " 가 기계를 작동시킵니다.");
+	}
+}
+
+class Main{
+	
+}
+public class Robot {
+	public static void main(String[] args) {
+		Robot_imp robot_imp = new Robot_imp("로보");
+		Movable movableRobotMovable = robot_imp;
+		
+		movableRobotMovable.move();
+		Activable activableRobotActivable = robot_imp;
+		activableRobotActivable.activate();
+		
+	}//end of main
+}
