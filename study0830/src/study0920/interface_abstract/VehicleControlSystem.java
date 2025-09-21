1f7adc1992595b8fb8008fe44ded6710d16aadcc
papa <papa@LAPTOP-G5DCD446>
2025-09-21 16:18:48 +0900

    개인테스트 스터디

--- study0830/src/study0920/interface_abstract/VehicleControlSystem.java
+++ study0830/src/study0920/interface_abstract/VehicleControlSystem.java
@@ -18,6 +18,37 @@ interface Driveable{
 	void turnRight();
 }
 
-public class VehicleControlSystem {
+class Car extends Vehicle implements Driveable{
+	
+	public Car (int speed) {
+		super(speed);
+	}
+	public void drive() {
+		System.out.println("자동차가 도로를 달립니다." + speed );
+	}
+	public void turnLeft() {
+		System.out.println("자동차가 좌회전 합니다.");
+	}
+	public void turnRight() {
+		System.out.println("자동차가 우회전 합니다.");
+	}
+}
 
+class Bicycle extends Vehicle{
+	public Bicycle (int speed) {
+		super(speed);
+	}
+	public void drive() {
+		System.out.println("자전거가 길을 달립니다.");
+	}
+	
+}
+
+public class VehicleControlSystem {
+	public static void main(String[] args) {
+		Car car = new Car(50);
+		car.drive();
+		car.accelerate();
+		car.turnLeft();
+	}
 }
